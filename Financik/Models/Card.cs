66fa81b27dd6902c27c5bf178ddbf50f17f08e04
rendellucky <antonkonev2003@gmail.com>
2023-11-01 20:18:40 +0200

    Add PersonalFinanceDb, PersonalFinanceDbContext and Models for PesonalFinance database.

---
+++ Financik/Models/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financik.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string Number { get; set; } = null!;
+        //Дата коли була створена карточка
+        public DateTime Date { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public List<Category> Categories { get; set; } = new();
+        public List<IncomeSource> IncomeSources { get; set; } = new();
+        public List<Cost> Costs { get; set; } = new();
+        public List<Income> Incomes { get; set; } = new();
+    }
+}
---
+++ Financik/Models/Income.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financik.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        //Кількість грошей зароблених за певний період
+        [Column(TypeName = "money")]
+        public decimal Count { get; set; }
+        //Дата з якої починається рахунок доходу
+        public DateTime DayFrom { get; set; }
+        //Дата до якої ведеться рахунок доходу
+        public DateTime DayTo { get; set; }
+        public int CardId { get; set; }
+        public Card? Card { get; set; }
+    }
+}
