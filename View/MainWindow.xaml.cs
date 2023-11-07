85e0c750ebc2fca232ba592c72c1791356904a74
ER <77330412+ertor031@users.noreply.github.com>
2023-11-07 23:45:57 +0200

    Update Main Window

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Financik.Data;
-using Financik.Db;
-using Financik.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,14 +20,9 @@ namespace Financik
     /// </summary>
     public partial class MainWindow : Window
     {
-        private PersonalFinanceDbContextFactory _factory = new PersonalFinanceDbContextFactory();
-        private PersonalFinanceDb db;
-        public User users;
-        public string[] args;
         public MainWindow()
         {
             InitializeComponent();
-            db = new PersonalFinanceDb(_factory.CreateDbContext(args));
             _ = MyTimer();
         }
 
@@ -58,12 +50,12 @@ namespace Financik
             }
         }
 
-        //private void Authorisation_Click(object sender, RoutedEventArgs e)
-        //{
+        private void Authorisation_Click(object sender, RoutedEventArgs e)
+        {
+            ///Код для входа 
+            ///Есле автризация успешка то add card enable = true
 
-        //    users =  db.GetUserByLoginAndPassword(MyLogin.Text, MyPassword.Password);
-       
-        //}
+        }
 
         private void Regestration_Click(object sender, RoutedEventArgs e)
         {
@@ -71,26 +63,11 @@ namespace Financik
             //////Есле регистрации успешка то add card enable = true
         }
 
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Statistic taskWindow = new Statistic();
-        //    taskWindow.ViewModel = "ViewModel";
-        //    taskWindow.Show();
-
-        //}
-
-        //private async void Regestration_Click_1(object sender, RoutedEventArgs e)
-        //{
-        //        await db.AddUserAsync(new Models.User { Login = MyLogin.Text, Password = MyPassword.Password });
-        //}
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
+            Statistic taskWindow = new Statistic();
+            taskWindow.ViewModel = "ViewModel";
+            taskWindow.Show();
 
         }
     }
